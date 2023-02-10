7edd8fc4caa8ea0822c9642baa50eb10ff32f27b
agailloty <mr.clerisaxel@hotmail.fr>
2023-02-10 16:59:57 +0100

    Recuperation infos offre d'emploi

--- projets/indeed_scraper/main.py
+++ projets/indeed_scraper/main.py
@@ -4,5 +4,16 @@ from lxml import html
 lien = "https://fr.indeed.com/jobs?q=data+analyst&l=France&from=searchOnHP&vjk=f400f062f379bb7b"
 save_html(lien, "pageIndeed.html")
 
-html_doc = html.fromstring(get_html_page(lien))
-print(html_doc.xpath("//span[starts-with(@id, 'jobTitle')]/text()"))
+# On peut récupérer le titre de l'offre d'emploi, le nom de l'entreprise, la note, 
+# la location, descriptif de l'offre d'emploi
+
+contenu_page = get_html_page(lien)
+html_doc = html.fromstring(contenu_page)
+
+
+job_titles = html_doc.xpath("//span[starts-with(@id, 'jobTitle')]/text()")
+company_names = html_doc.xpath("//a[@data-tn-element='companyName']/text()")
+company_rankings = html_doc.xpath("//span[@class='ratingsDisplay withRatingLink']") # A prendre avec
+# précaution car toutes les entreprises n'ont pas de note.
+job_locations = html_doc.xpath("//span[starts-with(@id, 'jobTitle')]/text()")
+job_descriptions = html_doc.xpath("//span[starts-with(@id, 'jobTitle')]/text()")
